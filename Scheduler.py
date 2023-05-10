f2d3462b743cf9f06c8575f0965e5b0f8f4d9b19
marios <t8200003@aueb.gr>
2023-05-10 22:27:25 +0300

    Change the way we check if a meeting is live

--- Scheduler.py
+++ Scheduler.py
@@ -4,9 +4,12 @@ import RedisConnection
 import MeetingInstance
 import datetime
 
+
 class Scheduler:
     meeting_instances = []
-    def get_last_minute_updated_meetings(self,):
+    redis = RedisConnection.connect_to_redis()
+
+    def get_last_minute_updated_meetings(self, ):
         con = DataBaseConnection.check_connection()
         if con is None:
             con = DataBaseConnection.connect_to_database()
@@ -21,21 +24,23 @@ class Scheduler:
             meetingID, orderID, fromdatetime, todatetime = meeting
             meet = MeetingInstance.MeetingInstance(meetingID, orderID, fromdatetime, todatetime)
             meetingInstaces.append(meet)
+            if self.redis.get(f'{meetingID}:{orderID}:status') is None:
+                self.redis.set(f'{meetingID}:{orderID}:status', 'inactive')
         return meetingInstaces
+
     def activate_meetings(self):
         # get all meeting instances with start time less than or equal to current time
         current_time = datetime.datetime.now()
         meetingsInstances = self.meeting_instances
-        redis = RedisConnection.connect_to_redis()
         for meetingInstance in meetingsInstances:
             meetingID, orderID = meetingInstance.meetingId, meetingInstance.orderId
             # set meeting instance status to "active"
-            if meetingInstance.fromdatatime <= current_time:
-                if not(redis.exists(f'{meetingID}:{orderID}:status', 'active')):
-                    redis.set(f'{meetingID}:{orderID}:status', 'active')
+            if meetingInstance.fromdatatime <= current_time <= meetingInstance.todatetime:
+                status = self.redis.get(f'{meetingID}:{orderID}:status')
+                if status.decode('utf-8') != "active":
+                    self.redis.set(f'{meetingID}:{orderID}:status', 'active')
                     print("Add to redis "
-                        "" + redis.get(f'{meetingID}:{orderID}:status').decode('utf-8'))
-
+                          "" + self.redis.get(f'{meetingID}:{orderID}:status').decode('utf-8'))
 
     def deactivate_meetings(self):
         # get all meeting instances with end time less than or equal to current time
@@ -46,17 +51,18 @@ class Scheduler:
             meetingID, orderID = meetingInstance.meetingId, meetingInstance.orderId
             # set meeting instance status to "inactive"
             if meetingInstance.todatetime <= current_time:
-                print(not (redis.exists(f'{meetingID}:{orderID}:status', 'inactive')))
-
-                if redis.exists(f'{meetingID}:{orderID}:status', 'active'):
+                status = redis.get(f'{meetingID}:{orderID}:status')
+                if status.decode('utf-8') != "inactive":
                     redis.set(f'{meetingID}:{orderID}:status', 'inactive')
                     print("Add to redis "
                           "" + redis.get(f'{meetingID}:{orderID}:status').decode('utf-8'))
+
     def getallmeetings(self):
         redis = RedisConnection.connect_to_redis()
         keys = redis.keys()
         for key in keys:
             print(key.decode('utf-8') + " " + redis.get(key).decode('utf-8'))
+
     def run(self):
         # Add all meetings to redis when the scheduler starts
         self.meeting_instances = self.get_all_meetings_instances()
@@ -88,5 +94,3 @@ class Scheduler:
 
 sc = Scheduler()
 sc.run()
-
-
